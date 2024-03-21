ea537c854e96ec1c441a3f5ec2a666788bad23f1
Francisco <francisco.carvalho.q@gmail.com>
2024-03-21 15:56:17 -0300

    Alterações iniciais

--- App.tsx
+++ App.tsx
@@ -1,18 +1,103 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import icon from './assets/images/icon.png';
+import { useState } from 'react';
 
 export default function App() {
+  const [mobName, setMobName] = useState('');
+  const [nameList, setNameList] = useState<string[]>([]);
+
+  const clearMobName = () => {
+    setMobName('');
+  };
+
+  const addMobName = () => {
+    setNameList([...nameList, mobName]);
+    clearMobName();
+  };
+
+  const removeMobNameByIndex = (index: number) => {
+    setNameList(nameList.filter((_, thisIndex) => thisIndex !== index));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.bgContainer}>
+      <View style={styles.column}>
+        <View style={styles.column}>
+          <center>
+            <Image style={styles.icon} source={icon} />
+          </center>
+          <View style={styles.column}>
+            <Text style={styles.text}>Cadastre seu mob:</Text>
+            <View style={styles.row}>
+              <Text style={styles.text}>Nome:</Text>
+              <TextInput style={styles.textInput} onChangeText={(name) => setMobName(name)} value={mobName} />
+            </View>
+            <View style={styles.rowBetween}>
+              <Button title='Limpar' onPress={clearMobName} />
+              <Button title='Adicionar' onPress={addMobName} />
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.column}>
+          {
+            nameList.map((name, index) => {
+              return <>
+                <View style={styles.mobNameContainer}>
+                  <Text style={styles.text}>
+                    {name}
+                  </Text>
+                  <Button title='Apagar' onPress={() => removeMobNameByIndex(index)}></Button>
+                </View>
+              </>
+            })
+          }
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  bgContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'darkgrey',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 20,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  mobNameContainer: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 2,
+    height: 30,
+  },
+  rowBetween: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  row: {
+    flexDirection: 'row',
+    gap: 20,
+  },
+  column: {
+    flexDirection: 'column',
+    gap: 20,
+  },
+  icon: {
+    width: 200,
+    height: 200,
   },
 });
